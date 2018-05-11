d6712d72988fa9488e922fd6e2d473442bfe1cf1
Libish Jacob <jajac@vestas.com>
2018-05-11 17:14:46 +0530

    Attachmnets are handled.
    Attachmnets are handled.

--- BotApplication/BotApplication/Dialogs/RootDialog.cs
+++ BotApplication/BotApplication/Dialogs/RootDialog.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
@@ -19,11 +20,25 @@ namespace BotApplication.Dialogs
         {
             var activity = await result as Activity;
 
-            // Calculate something for us to return
-            int length = (activity.Text ?? string.Empty).Length;
+            if (activity.Text != null)
+            {
+                // Calculate something for us to return
+                int length = (activity.Text ?? string.Empty).Length;
 
-            // Return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+                // Return our reply to the user
+                await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+            }
+            else if (activity.Attachments.Any())
+            {
+                string attachment = string.Empty;
+                foreach (var item in activity.Attachments)
+                {
+                    attachment += item.Name + ",";
+                }
+
+                // Return our reply to the user
+                await context.PostAsync($"You sent attachment(s) {attachment}");
+            }
 
             context.Wait(MessageReceivedAsync);
         }
--- BotApplication/Readme.txt
+++ BotApplication/Readme.txt
@@ -1,3 +1,4 @@
 you need visual studio template https://marketplace.visualstudio.com/items?itemName=BotBuilder.BotBuilderV3
 You need an emulator. https://docs.microsoft.com/en-us/azure/bot-service/bot-service-debug-emulator?view=azure-bot-service-3.0
 
+You have to use IE as the browser while running from Visual studio. Other browser was casing a problem wih the communication at the time of testing.
